68cc361c10d27492ae9a43dddca65a1132e11d8b
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-04 15:46:33 +0200

    script for running test and prod server

---
+++ runningscript.sh
@@ -0,0 +1,21 @@
+#Deploy final project(ex4) into the main servers (Test and Prod)
+
+#Vars
+HOME_DIR="/home/ubuntu"
+JENKINS_PIPE="/var/lib/jenkins/workspace/testpipe"
+SSH_KEY_TEST="${HOMEDIR}/.ssh/testserver"
+SSH_KEY_PROD="${HOME_DIR}/.ssh/prodserver"
+
+machine=$1
+
+#check args and using massages
+#check edge cases
+
+echo "deploying to $machine"
+echo "creating project dir"
+ssh -i "${SSH_KEY_TEST}" ${machine} "mkdir -p ${HOME_DIR}/final-project"
+echo "copying compose file to $machine"
+ssh -i "${SSH_KEY_TEST}" ${machine} "mkdir -p ${HOME_DIR}/final-project"
+echo "starting project"
+
+ssh -i "${SSH_KEY_TEST}" ${machine} docker version; docker compose version; jq --version
