20f362683181d67d65caaa10a39eb0b0edba8f3d
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-04 18:48:32 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -3,8 +3,8 @@
 #Vars
 HOME_DIR="/home/ubuntu"
 JENKINS_PIPE="/var/lib/jenkins/workspace/testpipe"
-SSH_KEY_TEST="${HOME_DIR}/.ssh/testserver"
-SSH_KEY_PROD="${HOME_DIR}/.ssh/prodserver"
+SSH_KEY_TEST="/var/lib/jenkins/.ssh/newkey"
+SSH_KEY_PROD="/var/lib/jenkins/.ssh/prodserver"
 
 machine=$1
 
