1eb4c4b396e5e652bc1fbec3b11d3ae4369cc3d1
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-05 17:12:32 +0200

    Update testfile.sh

--- testfile.sh
+++ testfile.sh
@@ -1,5 +1,10 @@
 
 #!/bin/bash
+################
+#testfile.
+################
+
+#check if files are located in machine 
 filename="docker-compose.yml"
 if test -f "$filename";
 then
@@ -7,8 +12,14 @@ then
 else
     echo "$filename has not been found"
 fi
-ping -c 1 testserver echo "ping to server failed" || echo "ping to server success"
 
+
+#check curl to server.
+ping -c 1 localhost:5000 echo "ping to server failed" || echo "ping to server success"
+
+
+
+#check if containers are live.
 CONTAINER_NAME_APP='ec2-user_app_1'
 CONTAINER_NAME_DB='ec2-user_db_1'
 CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME_APP}$)
