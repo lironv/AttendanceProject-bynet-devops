c33781f2ab05ba9ac048bcc999b6a68a7bdfe7d2
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-05 12:16:15 +0200

    Update testfile.sh

--- testfile.sh
+++ testfile.sh
@@ -8,3 +8,15 @@ else
     echo "$filename has not been found"
 fi
 ping -c 1 127.0.0.1:5000 &> /dev/null && echo success || echo fail
+
+CONTAINER_NAME_APP='ec2-user_app_1'
+CONTAINER_NAME_DB='ec2-user_db_1'
+CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME_APP}$)
+if [  "${CID}" ]; then
+  echo "Container exists, success"
+fi
+CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME_DB}$)
+if [  "${CID}" ]; then
+  echo "Container exists, success"
+fi
+unset CID
