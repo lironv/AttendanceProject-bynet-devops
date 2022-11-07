e9d1953cb9a00ee2b167452da73dea28d44b3d95
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-07 17:19:33 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -16,10 +16,10 @@ machine=$1
 echo "deploying to $machine"
 echo "creating project dir"
 
-if [[${machine} == "prodserver" ]]; then
-scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no -r "${WORKSPACE}/db" ec2-user@${machine}:
-scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no "${WORKSPACE}/docker-compose.yml" $ENVFILE_LOCATION ec2-user@${machine}:
-ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@${machine} "docker login; docker-compose up -d; sleep 5"
+if [ $machine == "prodserver" ]; then
+scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no -r "$WORKSPACE/db" ec2-user@$machine:
+scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no "$WORKSPACE/docker-compose.yml" $ENVFILE_LOCATION ec2-user@$machine:
+ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@$machine "docker login; docker-compose up -d; sleep 5"
 fi
 
 
@@ -27,11 +27,8 @@ fi
 
 
 #if the machine is test, copying and running tests on test-server 
-if [[${machine} == "testserver" ]]; then
-   scp -r "${WORKSPACE}/db" ec2-user@${machine}:
-   scp "${WORKSPACE}/docker-compose.yml" "${WORKSPACE}/testfile.sh" $ENVFILE_LOCATION ec2-user@${machine}:
-   ssh ec2-user@${machine} "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 100"
-   ssh ec2-user@${machine} "chmod u+x ./testfile.sh"
-   ssh ec2-user@${machine} "./testfile.sh"
-   ssh ec2-user@${machine} "docker-compose down; docker volume prune -f"
+if [ $machine == "testserver" ]; then
+   scp -r "$WORKSPACE/db" ec2-user@$machine:
+   scp "$WORKSPACE/docker-compose.yml" "$WORKSPACE/testfile.sh" $ENVFILE_LOCATION ec2-user@$machine:
+   ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 100;chmod u+x ./testfile.sh; ./testfile.sh; docker-compose down; docker volume prune -f"
 fi
