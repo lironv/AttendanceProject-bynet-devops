8c0d6574b9b0d322ef35cb99a72db21edeaab90a
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-07 17:03:25 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -6,6 +6,7 @@ JENKINS_PIPE="/var/lib/jenkins/workspace/testpipe"
 SSH_KEY_TEST="${HOME_DIR}/.ssh/testserver"
 SSH_KEY_PROD="/var/lib/jenkins/.ssh/prodserver"
 
+#machine names are testserver, prodserver
 machine=$1
 
 #check args and using massages
@@ -15,10 +16,22 @@ machine=$1
 echo "deploying to $machine"
 echo "creating project dir"
 
-scp -r "${WORKSPACE}/db" ec2-user@testserver:
-scp "${WORKSPACE}/docker-compose.yml" "${WORKSPACE}/testfile.sh" $ENVFILE_LOCATION ec2-user@testserver:
-ssh ec2-user@testserver "mkdir app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 100"
-ssh ec2-user@testserver "chmod u+x ./testfile.sh"
-ssh ec2-user@testserver "./testfile.sh"
-ssh ec2-user@testserver "docker-compose down; docker volume prune -f"
+if [[$machine == "prodserver" ]]; then
+scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no -r "${WORKSPACE}/db" ec2-user@$machine:
+scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no "${WORKSPACE}/docker-compose.yml" $ENVFILE_LOCATION ec2-user@$machine:
+ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@$machine "docker login; docker-compose up -d; sleep 5"
+fi
 
+
+
+
+
+#if the machine is test, copying and running tests on test-server 
+if [[$machine == "testserver" ]]; then
+   scp -r "${WORKSPACE}/db" ec2-user@$machine:
+   scp "${WORKSPACE}/docker-compose.yml" "${WORKSPACE}/testfile.sh" $ENVFILE_LOCATION ec2-user@$machine:
+   ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 100"
+   ssh ec2-user@$machine "chmod u+x ./testfile.sh"
+   ssh ec2-user@$machine "./testfile.sh"
+   ssh ec2-user@$machine "docker-compose down; docker volume prune -f"
+fi
