214d8406ddc491dbaf4b481baac816ce7ff503b5
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-09 18:05:54 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -18,20 +18,19 @@ echo "creating project dir"
 
 if [ $machine == "prodserver" ]; then
 rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/prodserver" ec2-user@$machine:/$HOME_DIR/db "$WORKSPACE/db"
-rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/prodserver" ec2-user@$machine:/docker-compose.yml $ENVFILE_LOCATION $WORKSPACE/docker-compose.yml
+rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/prodserver" ec2-user@$machine:/docker-compose.yml $WORKSPACE/docker-compose.yml
 
 ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@$machine "mkdir -p app;docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 5"
 fi
 
 
 
- rsync /home/tin/file2.txt /home/tin/file3.txt /home/tin/Documents
 
 #if the machine is test, copying and running tests on test-server 
 if [ $machine == "testserver" ]; then
    rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/$HOME_DIR/db "$WORKSPACE/db"
-   rsync -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/ $ENVFILE_LOCATION $WORKSPACE/docker-compose.yml
-   rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/ $ENVFILE_LOCATION $WORKSPACE/docker-compose.yml $WORKSPACE/testfile.sh
+   rsync -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/  $WORKSPACE/docker-compose.yml
+   rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/  $WORKSPACE/docker-compose.yml $WORKSPACE/testfile.sh
    
    ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build;
        sleep 70;bash ./testfile.sh; docker-compose down; docker volume prune -f"
