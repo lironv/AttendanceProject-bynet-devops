52c73003f4cea328d92e6212f527a8d1d1d9c652
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-10 18:32:25 +0200

    Update testfile.sh

--- testfile.sh
+++ testfile.sh
@@ -1,39 +1,39 @@
 
-#!/bin/bash
+#!/bin/bash -xe
 ################
 #testfile .
 ################
 
 #check if files are located in machine 
 filename="docker-compose.yml"
-if test -f "$filename";
-then
-    echo "$filename has found."
-else
-    echo "$filename has not been found"
-fi
+#if test -f "$filename";
+#then
+#    echo "$filename has found."
+#else
+#    echo "$filename has not been found"
+#fi
 
 
 #check curl to server.
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:5001)
 
-if [[ "$status_code" -ne 200 ]] ; then
-  echo "Site status changed to $status_code"
-  exit 1
-else
-  echo "successful task, curl localhost:5000"
-fi
+#if [[ "$status_code" -ne 200 ]] ; then
+#  echo "Site status changed to $status_code"
+#  exit 1
+#else
+#  echo "successful task, curl localhost:5000"
+#fi
 
 
 #check if containers are live.
 CONTAINER_NAME_APP='ec2-user_app_1'
 CONTAINER_NAME_DB='ec2-user_db_1'
 CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME_APP}$)
-if [  "${CID}" ]; then
-  echo "Container exists, success"
-fi
+#if [  "${CID}" ]; then
+#  echo "Container exists, success"
+#fi
 CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME_DB}$)
-if [  "${CID}" ]; then
-  echo "Container exists, success"
-fi
+#if [  "${CID}" ]; then
+#  echo "Container exists, success"
+#fi
 unset CID
