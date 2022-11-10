142b5f02d4c211d52e4c0d45010a6410f9f227b5
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-10 18:34:20 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -24,14 +24,11 @@ ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@$machine "mkdir -p app;docker
 fi
 
 
-
-
-#if the machine is test, copying and running tests on test-server 
 if [ $machine == "testserver" ]; then
-   rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/$HOME_DIR/db "$WORKSPACE/db"
-   rsync -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/  $WORKSPACE/docker-compose.yml
-   rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/testserver" ec2-user@$machine:/  $WORKSPACE/docker-compose.yml $WORKSPACE/testfile.sh
-   
+   scp -r "$WORKSPACE/db" ec2-user@$machine:
+   scp "$WORKSPACE/docker-compose.yml" "$WORKSPACE/testfile.sh" $ENVFILE_LOCATION ec2-user@$machine:
+   ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 70;chmod u+x ./testfile.sh; ./testfile.sh; docker-compose down; docker volume prune -f"
    ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build;
        sleep 70;bash ./testfile.sh; docker-compose down; docker volume prune -f"
 fi
+
