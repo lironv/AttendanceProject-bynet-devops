d7b73f87a838ecbf2b03d3c7db3f97d7f0148a71
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-14 15:27:25 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -11,9 +11,9 @@ machine=$1
 echo "deploying to $machine"
 echo "creating project dir"
 if [ $machine == "prodserver" ]; then
-rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/prodserver" ec2-user@$machine:/$HOME_DIR/db "$WORKSPACE/db"
-rsync -Pav -e "ssh -i /var/lib/jenkins/.ssh/prodserver" ec2-user@$machine:/docker-compose.yml $WORKSPACE/docker-compose.yml
-ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@$machine "mkdir -p app;docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 5"
+scp -r "$WORKSPACE/db" ec2-user@$machine:
+scp "$WORKSPACE/docker-compose.yml" $ENVFILE_LOCATION ec2-user@$machine:
+ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@$machine "mkdir -p app;docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 70"
 fi
 
 
@@ -24,5 +24,5 @@ if [ $machine == "testserver" ]; then
    scp -r "$WORKSPACE/db" ec2-user@$machine:
    scp "$WORKSPACE/docker-compose.yml" "$WORKSPACE/testfile.sh" $ENVFILE_LOCATION ec2-user@$machine:
    ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build;
-       sleep 70;set -e; bash testfile.sh; docker-compose down; docker volume prune -f"
+       sleep 70;set -e; bash testfile.sh;sleep 5; docker-compose down;sleep 5; docker image rm lironv/attendance:latest; docker volume prune -f"
 fi
