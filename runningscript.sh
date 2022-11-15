d9aa7a9882ef3cbf54a2224d9080709205f18e56
liron vaknin <45284793+lironv@users.noreply.github.com>
2022-11-15 20:26:08 +0200

    Update runningscript.sh

--- runningscript.sh
+++ runningscript.sh
@@ -6,13 +6,16 @@ SSH_KEY_TEST="${HOME_DIR}/.ssh/testserver"
 SSH_KEY_PROD="/var/lib/jenkins/.ssh/prodserver"
 #machine names are testserver, prodserver
 machine=$1
-#check args and using massages
-#check edge cases
+
+
+
 echo "deploying to $machine"
 echo "creating project dir"
 if [ $machine == "prodserver" ]; then
-   scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no -r "${WORKSPACE}/db" ec2-user@prodserver:
-   scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no "${WORKSPACE}/docker-compose.yml" $ENVFILE_LOCATION ec2-user@prodserver:
+   echo "starting scp process"
+   scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no -r -v "${WORKSPACE}/db" ec2-user@prodserver:
+   scp -i "/var/lib/jenkins/.ssh/prodserver" -o StrictHostKeyChecking=no -v "${WORKSPACE}/docker-compose.yml" $ENVFILE_LOCATION ec2-user@prodserver:
+   echo "start prod environment, deploying site"
    ssh -i "/var/lib/jenkins/.ssh/prodserver" ec2-user@$machine "mkdir -p app;docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build; sleep 70"
 fi
 
@@ -23,6 +26,9 @@ fi
 if [ $machine == "testserver" ]; then
    scp -r "$WORKSPACE/db" ec2-user@$machine:
    scp "$WORKSPACE/docker-compose.yml" "$WORKSPACE/testfile.sh" $ENVFILE_LOCATION ec2-user@$machine:
-   ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build;
-       sleep 70;set -e; bash testfile.sh;sleep 5; docker-compose down;sleep 5; docker image rm lironv/attendance:latest; docker volume prune -f"
+   echo "start test environment, deploying site and testing"
+   ssh ec2-user@$machine "mkdir -p app; docker login;docker pull lironv/attendance:latest; docker-compose up -d --no-build;sleep 70"
+   echo "start testing process"
+   ssh ec2-user@$machine "set -e; bash testfile.sh;sleep 5; docker-compose down;sleep 5; docker image rm lironv/attendance:latest; docker volume prune -f"
+   
 fi
